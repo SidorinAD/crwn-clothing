6503ce37ecab30c178bfa5c31ae9e32df5185f44
Alexander Sidorin <a.d.sidorin@gmail.com>
2020-10-29 19:43:05 +0300

    updating stripe integrartion and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HhYzuLMVY1m2rNHveEuq4joIYY3omVVnyPDgUBe6D2olREuNDoWiVzPiBMXFGdcDvHsfoUerg4himHnT8d18pNc00yTZR7OpT'
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name= 'CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
